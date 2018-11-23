e2304693e0b22fcb562c84043c43c33e9fb15897
Xavier Decuyper <hi@savjee.be>
2018-11-23 11:59:20 +0100

    Isolate the Block view and put it in its own component

--- src/app/components/block-view/block-view.component.ts
+++ src/app/components/block-view/block-view.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { BlockchainService } from '../../services/blockchain.service';
 
 @Component({
   selector: 'app-block-view',
@@ -6,10 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./block-view.component.scss']
 })
 export class BlockViewComponent implements OnInit {
+	@Input()
+	public block;
 
-  constructor() { }
+	@Input()
+	public selectedBlock;
+
+	private blocksInChain;
+
+  constructor(private blockchainService: BlockchainService) {
+  	this.blocksInChain = blockchainService.blockchainInstance.chain;
+  }
+
 
   ngOnInit() {
+  }
+
+  blockHasTx(){
+  	return this.block.transactions.length > 0;
+  }
+
+  isSelectedBlock(){
+  	return this.block === this.selectedBlock;
+  }
+
+  getBlockNumber(){
+  	return this.blocksInChain.indexOf(this.block) + 1;
   }
 
 }
