e50e00f5e220c6760e1482628241523284e69360
will1129 <mwilliams8979@gmail.com>
2021-03-13 20:27:21 -0500

    Test Data Available to be loaded

--- src/app/pages/blockchain-viewer/blockchain-viewer.component.ts
+++ src/app/pages/blockchain-viewer/blockchain-viewer.component.ts
@@ -1,17 +1,20 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { BlockchainService } from '../../services/blockchain.service';
+import $ from 'jquery';
+
 @Component({
   selector: 'app-blockchain-viewer',
   templateUrl: './blockchain-viewer.component.html',
   styleUrls: ['./blockchain-viewer.component.scss']
 })
 export class BlockchainViewerComponent implements OnInit {
-
   public blocks = [];
+  private blockchain;
   public selectedBlock = null;
 
   constructor(private blockchainService: BlockchainService) {
     this.blocks = blockchainService.blockchainInstance.chain;
+    this.blockchain = blockchainService.blockchainInstance;
     this.selectedBlock = this.blocks[0];
     console.log(this.blocks);
   }
@@ -19,24 +22,29 @@ export class BlockchainViewerComponent implements OnInit {
   ngOnInit() {
   }
 
+  loadTestData() {
+    this.blockchainService.loadTestData();
+  }
+
+  // Logs the block passed in
+  // Sets the selected block to the block passed in
   showTransactions(block) {
     console.log(block);
     this.selectedBlock = block;
     return false;
   }
 
+  // Returns if the block passed in has any transactions
   blockHasTx(block) {
     return block.transactions.length > 0;
   }
 
-  selectedBlockHasTx() {
-    return this.blockHasTx(this.selectedBlock);
-  }
-
+  // Tells us if the blocked passed in is the selected block
   isSelectedBlock(block) {
     return this.selectedBlock === block;
   }
 
+  // Returns the (index + 1) of the block in the blockchain array
   getBlockNumber(block) {
     return this.blocks.indexOf(block) + 1;
   }
--- src/app/services/blockchain.service.ts
+++ src/app/services/blockchain.service.ts
@@ -8,6 +8,7 @@ import EC from 'elliptic';
 export class BlockchainService {
   public blockchainInstance = new Blockchain(); // Initializes the blockchain
   public wallets: Array<IWalletKey> = []; // Creates array of empty wallets so user can have multiple wallets
+  public testTxns: Array<Transaction> = []; // Creates array of empty transactions to use for test data
 
   // Sets default difficulty of blocks to 2 for miners in the blockchain
   // Automatically mines the first block and rewards address 'hi' with the mining reward
@@ -20,6 +21,44 @@ export class BlockchainService {
     for (let w = 0; w < usersNum; w++) { // Generates wallets for each user
       this.generateWalletKeys();
     }
+  }
+
+  // Generates random data tto 
+  loadTestData() {
+    let loadingTestData = false;
+    let usersNum = 10; // Sets total user count
+
+    let blockNum = Math.floor(Math.random() * 10) + 1;  // Create a random number of blocks from 1 to 10
+
+    for (let i = 0; i < blockNum; i++) { // Block loop
+      let txnNum = Math.floor(Math.random() * 50) + 1;  // Create a random number of txns from 1 to 100
+      this.testTxns = new Array<Transaction>();
+
+      for (let j = 0; j < txnNum; j++) { // Transaction loop
+        let tx = new Transaction();
+        let fromWalletNum = Math.floor(Math.random() * usersNum);
+        let toWalletNum = Math.floor(Math.random() * usersNum);
+
+        while (toWalletNum == fromWalletNum) {
+          toWalletNum = Math.floor(Math.random() * usersNum);
+        }
+
+        this.testTxns[j] = tx;
+        this.testTxns[j].fromAddress = this.wallets[fromWalletNum].publicKey;
+        this.testTxns[j].toAddress = this.wallets[toWalletNum].publicKey;
+        this.testTxns[j].amount = Math.floor(Math.random() * 50) + 1; // Generate a random amount from 1 to 100
+        this.testTxns[j].signTransaction(this.wallets[fromWalletNum].keyObj);
+        
+        try {
+          this.addTransaction(this.testTxns[j]);
+        } catch (e) {
+          alert(e);
+        }
+      }
+      this.minePendingTransactions();
+    }
+
+    loadingTestData = true;;
   }
 
   // Creates a new transaction for the reward that will be given to who successfully mines the block
