225450d7a1c9676c54476c1f360dd1a30ad22898
will1129 <mwilliams8979@gmail.com>
2021-03-13 20:06:08 -0500

    Added wallet updates
    - Generates 10 random wallets
    - User is always the first wallet
    - Mining reward is given to random user each time
    -

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { TransactionsTableComponent } from './components/transactions-table/tran
 import { CreateTransactionComponent } from './pages/create-transaction/create-transaction.component';
 import { PendingTransactionsComponent } from './pages/pending-transactions/pending-transactions.component';
 import { WalletBalanceComponent } from './pages/wallet-balance/wallet-balance.component';
+import { WalletComponent } from './pages/wallet/wallet.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,8 @@ import { WalletBalanceComponent } from './pages/wallet-balance/wallet-balance.co
     TransactionsTableComponent,
     CreateTransactionComponent,
     PendingTransactionsComponent,
-    WalletBalanceComponent
+    WalletBalanceComponent,
+    WalletComponent
   ],
   imports: [
     BrowserModule,
